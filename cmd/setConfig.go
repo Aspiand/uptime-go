0a7c017e665851fac98b5a35d0822275417252c3
Aspian <muhammad.aspian.d@gmail.com>
2025-08-21 14:36:52 +0800

    fix yaml config

--- cmd/setConfig.go
+++ cmd/setConfig.go
@@ -13,12 +13,12 @@ import (
 
 type MonitorConfig struct {
 	Monitor []struct {
-		URL                      string `json:"url"`
-		Enabled                  bool   `json:"enabled"`
-		ResponseTimeThreshold    string `json:"response_time_threshold"`
-		Interval                 string `json:"interval"`
-		CertificateMonitoring    bool   `json:"certificate_monitoring"`
-		CertificateExpiredBefore string `json:"certificate_expired_before"`
+		URL                      string `json:"url" yaml:"url"`
+		Enabled                  bool   `json:"enabled" yaml:"enabled"`
+		ResponseTimeThreshold    string `json:"response_time_threshold" yaml:"response_time_threshold"`
+		Interval                 string `json:"interval" yaml:"interval"`
+		CertificateMonitoring    bool   `json:"certificate_monitoring" yaml:"certificate_monitoring"`
+		CertificateExpiredBefore string `json:"certificate_expired_before" yaml:"certificate_expired_before"`
 	} `json:"monitor"`
 }
 
@@ -50,6 +50,8 @@ var setConfigCmd = &cobra.Command{
 			}.Print()
 			os.Exit(1)
 		}
+
+		fmt.Fprint(os.Stderr, yamlConfig)
 
 		err = os.WriteFile(configuration.Config.ConfigFile, yamlConfig, 0644)
 		if err != nil {
